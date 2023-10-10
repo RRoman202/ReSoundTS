7c7cb7c14e5087c6b4686e6cfe81d28e282d1429
RRoman202 <romanrrr2005rrr@gmail.com>
2023-10-10 16:47:00 +0500

    update13

---
+++ src/components/progress.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { Prog } from "./Pages/Piano";
+
+function delay(time: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+const ProgressBar = () => {
+  const [position, setPosition] = useState<number>(0);
+
+  const moveComponent = () => {
+    setPosition((prevPosition) => prevPosition + 1.35);
+    requestAnimationFrame(moveComponent);
+  };
+
+  const handleStartMoving = async () => {
+    await delay(500);
+    requestAnimationFrame(moveComponent);
+  };
+
+  return (
+    <div style={{ position: "relative", height: "200px" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: position,
+          width: "2px",
+          height: "3400px",
+          backgroundColor: "blue",
+        }}
+      ></div>
+      <Prog handleStartMoving={handleStartMoving}></Prog>
+    </div>
+  );
+};
+
+export default ProgressBar;
